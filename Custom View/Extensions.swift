6c789d9d22155311095e15081842a73271f327e1
Marquavious Draggon <marq.draggon@gmail.com>
2016-12-09 11:26:50 -0800

    fixed border extension

--- Custom View/Extensions.swift
+++ Custom View/Extensions.swift
@@ -16,24 +16,24 @@ extension CALayer {
         let border = CALayer()
         
         switch edge {
-        case UIRectEdge.top:
-            // border.frame = CGRect(0, 0, self.frame.height, thickness)
-            border.frame = CGRect(x: 0, y: 0, width: self.frame.height, height: thickness)
-            break
-        case UIRectEdge.bottom:
-            // border.frame = CGRect(0, CGRectGetHeight(self.frame) - thickness, UIScreen.main.bounds.width, thickness)
-            border.frame = CGRect(x: 0, y: self.frame.height - thickness, width: UIScreen.main.bounds.width, height: thickness)
-            break
-        case UIRectEdge.left:
-            // border.frame = CGRect(0, 0, thickness, self.frame.height)
-            border.frame = CGRect(x: 0, y: 0, width: thickness, height: self.frame.height)
-            break
-        case UIRectEdge.right:
-            // border.frame = CGRect(self.frame.width - thickness, 0, thickness, CGRectGetHeight(self.frame))
-            border.frame = CGRect(x: self.frame.width - thickness, y: 0, width: thickness, height: self.frame.height)
-            break
-        default:
-            break
+            case UIRectEdge.top:
+                // border.frame = CGRect(0, 0, self.frame.height, thickness)
+                border.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: thickness)
+                break
+            case UIRectEdge.bottom:
+                // border.frame = CGRect(0, CGRectGetHeight(self.frame) - thickness, UIScreen.main.bounds.width, thickness)
+                border.frame = CGRect(x: 0, y: self.frame.height - thickness, width: self.frame.width, height: thickness)
+                break
+            case UIRectEdge.left:
+                // border.frame = CGRect(0, 0, thickness, self.frame.height)
+                border.frame = CGRect(x: 0, y: 0, width: thickness, height: self.frame.height)
+                break
+            case UIRectEdge.right:
+                // border.frame = CGRect(self.frame.width - thickness, 0, thickness, CGRectGetHeight(self.frame))
+                border.frame = CGRect(x: self.frame.width - thickness, y: 0, width: thickness, height: self.frame.height)
+                break
+            default:
+                break
         }
         
         border.backgroundColor = color.cgColor;
